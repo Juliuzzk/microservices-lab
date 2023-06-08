47b2851de74747d5d3b2d62ae80c1267f68502e4
Julio Esteban Cáceres Sanzana <Julio@Macbook-M1-Pro.local>
2023-06-08 01:25:19 -0400

    Correcciones basicas, y redaccion de Readme

--- 03-servicios-frontend/frontend-api/src/controllers/custodyController.ts
+++ 03-servicios-frontend/frontend-api/src/controllers/custodyController.ts
@@ -1,69 +1,70 @@
 import { Request, Response } from "express";
-import * as grpc from '@grpc/grpc-js';
-import * as protoLoader from '@grpc/proto-loader';
+import * as grpc from "@grpc/grpc-js";
+import * as protoLoader from "@grpc/proto-loader";
 
-const PROTO_PATH = __dirname + '../../../protos/system/custody/custody.proto';
+const PROTO_PATH = __dirname + "../../../protos/system/custody/custody.proto";
 
 const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
-	keepCase: true,
-	defaults: true,
-	oneofs: true
+  keepCase: true,
+  defaults: true,
+  oneofs: true,
 });
 
 const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
 
-const endpoint = process.env.BILLING_BACKEND || 'custody-service.backend:5001';
+const endpoint = process.env.BILLING_BACKEND || "custody-service.backend:5000";
 
 const creds = grpc.credentials.createInsecure();
 const service = (protoDescriptor.lab as any).system.custody.CustodyService;
 let stub = new service(endpoint, creds);
 
 export const add = async (req: Request, res: Response) => {
+  const msg = req.body;
 
-	const msg = req.body;
+  console.log(req.body);
 
-	console.log(req.body)
+  const now = Math.floor(Date.now() / 1000);
 
-	const now = Math.floor(Date.now() / 1000);
+  const p = new Promise((resolve, reject) =>
+    stub.AddCustodyStock(
+      {
+        period: msg.period,
+        client_id: msg.client_id,
+        stock: msg.stock,
+        quantity: msg.quantity,
+      },
+      (err: any, response: any) => {
+        if (err) return reject(err);
+        resolve(response);
+      }
+    )
+  );
 
-	const p = new Promise((resolve, reject) =>
-		stub.AddCustodyStock({
-			period: msg.period,
-			client_id: msg.client_id,
-			stock: msg.stock,
-			quantity: msg.quantity
-		}, (err: any, response: any) => {
-			if (err)
-				return reject(err);
-			resolve(response);
-		})
-	);
+  const result = await p;
 
-	const result = await p;
-
-	return res.json(result);
-}
+  return res.json(result);
+};
 
 export const get = async (req: Request, res: Response) => {
+  console.log("GET:");
+  console.log(req.body);
+  const msg = req.body;
 
-	console.log("GET:")
-	console.log(req.body)
-	const msg = req.body;
+  const p = new Promise((resolve, reject) =>
+    stub.GetCustody(
+      {
+        period: msg.period,
+        client_id: msg.client_id,
+        stock: msg.stock,
+      },
+      (err: any, response: any) => {
+        if (err) return reject(err);
+        resolve(response);
+      }
+    )
+  );
 
-	const p = new Promise((resolve, reject) =>
-		stub.GetCustody({
-			period: msg.period,
-			client_id: msg.client_id,
-			stock: msg.stock,
-		}, (err: any, response: any) => {
-			if (err)
-				return reject(err);
-			resolve(response);
-		})
-	);
+  const result = await p;
 
-	const result = await p;
-
-	return res.json(result);
-}
-
+  return res.json(result);
+};
--- README.md
+++ README.md
@@ -1,11 +1,90 @@
-# microservices-lab
-Laboratorio de microservicios
 
-## Instrucciones
+# Entrega Evaluación - Desarrollo de taller Microservicios
 
-Ingresar a la carpeta __01-kubernetes__ y ejecutar scripts `00-create-cluster.sh` y `01-create-backend.sh`
+## Integrantes
+
+- Cristian Simon
+- Juan Jara
+- Julio Cáceres
+- Sebastian Hechenleitner
+
+##  Levantar proyecto
+
+1. __Clonar__ o realizar __fork__ del repositorio presente. Para clonar el repositorio, ejecutar el siguiente comando:
+
+```shell
+cd <directorio de trabajo>
+git clone https://github.com/malarcon-79/microservices-lab.git
+cd microservices-lab
+```
+
+2. Iniciar la base de datos y otros requisitos. Para esto, se debe ingresar a la carpeta __01-kubernetes__ y ejecutar scripts `00-create-cluster.sh` y `01-create-backend.sh`
+
 ```shell
 cd 01-kubernetes
+
 ./00-create-cluster.sh
 ./01-create-backend.sh
+
+cd ..
 ```
+
+3. Generar imagen de los servicios de backend, para esto debemos dirigirnos a a la carpeta `02-servicios-backend` y ejecutar el comando de docker build en las siguientes rutas:
+
+```shell
+cd 02-servicios-backend
+
+cd billing-service
+docker build -t billing-server:latest .
+
+cd ..
+
+cd custody-service
+docker build -t custody-server:latest .
+
+cd ../..
+```
+
+4. Generar imagen de los servicios de frontend, para esto debemos dirigirnos a la carpeta `03-servicios-frontend` y ejecutar el comando de docker build en las siguientes rutas:
+
+
+```shell
+cd 03-servicios-frontend
+
+cd frontend-api
+docker build -t frontend-api:latest .
+
+cd ..
+
+cd frontend-app
+docker build -t frontend-app:latest .
+
+cd ../..
+```
+
+5. Exportar las imagenes de docker recientemente creadas  al cluster de kubernetes:
+
+```shell
+
+k3d image import billing-service:latest -c dev
+
+k3d image import custody-service:latest -c dev
+
+k3d image import frontend-api:latest -c dev
+
+k3d image import frontend-app:latest -c dev
+
+```
+
+6. Cargar configuraciones a nuestro cluster de kubernetes:
+
+```shell
+cd k8s
+
+kubectl apply -f liberacion.yaml
+
+```
+
+7. Con los pasos realizados, ya podremos hacer uso de nuestro proyecto.
+
+---
